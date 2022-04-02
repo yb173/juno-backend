c1d70b291aa64987079daacc5250b8868e81d049
yb173 <56855963+yb173@users.noreply.github.com>
2022-04-02 11:12:40 +0900

    feat: ポートを env から取得するように変更

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    credentials: true,
+  });
+
   const configService = app.get(ConfigService);
-  await app.listen(configService.get('PORT') || 3000);
+  await app.listen(configService.get('PORT') || 6001);
 }
 
 bootstrap();
